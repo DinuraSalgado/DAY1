18959c3d28f0155c7eadd41ca4f6710a972d8de6
DinuraSalgado <dinura@qbitum.com>
2025-01-22 16:51:00 +0530

    Add files via upload

---
+++ Q5/src/Main.java
@@ -0,0 +1,24 @@
+
+class Triangle {
+    public static void RTriangle(int n)
+    {
+        int a, b;
+
+
+        for (a = 0; a < n; a++) {
+
+
+            for (b = 0; b <= a; b++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int k = 5;
+        RTriangle(k);
+    }
+}
